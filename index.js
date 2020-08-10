fb5a60892c2230678613f7fbc3be314405dc0094
kinawy <kinawy@Luna.localdomain>
2020-08-09 23:51:26 -0400

    [JS/EJS] I am the literal worst and always forget to add and commit, I will be better about this in the future :(

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios'); 
-const ejsLayouts = require('express-ejs-layouts');
+// variables for the the packages
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const ejsLayouts = require("express-ejs-layouts");
 const app = express();
 const port = process.env.PORT || 3000;
+const methodOverride = require('method-override');
 
-app.use(require('morgan')('dev'));
-app.set('view engine', 'ejs');
+// Imports all routes from the pokemon routes file
+app.use("/pokemon", require("./routes/pokemon"));
+app.use(methodOverride('_method'));
+app.use(require("morgan")("dev"));
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static("public"));
+
 
 // GET - main index of site
-app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+app.get("/", function (req, res) {
+  let pokemonUrl = "http://pokeapi.co/api/v2/pokemon?limit=151>/";
   // Use request to call the API
-  axios.get(pokemonUrl).then(response => {
+  axios.get(pokemonUrl).then((response) => {
+    console.log(response.data);
     let pokemon = response.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
+    res.render("index", { pokemon: pokemon.slice(0, 151) });
   });
 });
 
-// Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
 
 const server = app.listen(port, () => {
   console.log(`Server is running on ${port}`);
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,87 @@
-var express = require('express');
-var router = express.Router();
+// variable for express
+const express = require("express");
+// variable for the express router
+const router = express.Router();
+// variable for axios
+const axios = require("axios");
+// variable for my models aka database in psql
+const db = require("../models");
+
+
+
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+router.get("/", (req, res) => {
+  // get the records from my database, render to view
+  db.pokemon.findAll().then((poke) => {
+    // render my index.ejs page from my pokemon folder inside views, assign the pokemon to a pokemon object, // this is my favorites page
+    res.render("pokemon/index", { pokemon: poke });
+  });
+});
+
+// this one renders my show page, where I have a little crappy pokemon card
+router.get("/:id", (req, res) => {
+  // finds in my database where the id matches the request
+  db.pokemon
+    .findOne({
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((poke) => {
+      // then grab the url again and change their dataValues.name to lower case so they can be added on to the end of the API call, then I get it again inside another axios call, wwhere I do a then.
+      let pokeUrl =
+        "http://pokeapi.co/api/v2/pokemon/" +
+        poke.dataValues.name.toLowerCase();
+      axios
+        .get(pokeUrl)
+        .then((response) => {
+          // set a variable for the new data, render pokemon/show.ejs and give it the pokeData object to be used on the page
+          let pokeData = response.data;
+          console.log(pokeData.stats);
+
+          res.render("pokemon/show", { pokeData: pokeData });
+        })
+        // THIS ONE WAS SUCH A PAIN IN THE ASS, didn't realize I needed a second catch for so damn long, I spent more time on that than I did anything else
+        .catch((error) => {
+          console.log("Error", error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+router.post("/", (req, res) => {
+  // creates the pokemon in my database, gives it the name of req.body.name, which is its name
+  db.pokemon
+    .create({
+      name: req.body.name,
+    })
+    .then((poke) => {
+      console.log("Created: ", poke.name);
+      // then redirects the call to my favorites page
+      res.redirect("/pokemon");
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  // uses the destroy function to remove a pokemon from my database, not working though
+  db.pokemon
+    .destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((poke) => {
+      console.log("Deleted " + poke.name + " from favorites.");
+      // then redirects to the favorites page again
+      res.redirect("/pokemon");
+    })
+    .catch((error) => {
+      console.log("Error", error);
+    });
 });
 
 module.exports = router;
